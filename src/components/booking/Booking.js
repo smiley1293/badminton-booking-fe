31c75b9de0bb10ed7aedb20613db57e4f8836cbb
VantCii <viet12092003@gmail.com>
2024-07-18 02:44:10 +0700

    refactor + add api + router pages

---
+++ src/components/booking/Booking.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Booking = () => {
+  return (
+    <div>
+      Đây là trang booking
+    </div>
+  );
+};
+
+export default Booking;
---
+++ src/pages/Login.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Login = () => {
+  return (
+    <div>
+      Đây là trang login
+    </div>
+  );
+};
+
+export default Login;
